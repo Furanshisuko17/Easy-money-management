fc848dc7c628b2d5a9d165f4b6a604c1e6b205c6
Furanshisuko17 <darkfran555@gmail.com>
2022-03-02 20:09:31 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,16 @@
+import PyQt5.QtWidgets as qtw
 
+class MainWindow(qtw.QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hello")
+
+        self.show
+
+
+
+app = qtw.QApplication([])
+mw = MainWindow()
+
+app.exec_()
