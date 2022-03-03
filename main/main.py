d160619ac1d8483dc76dbe9527319e3be406ef51
Furanshisuko17 <darkfran555@gmail.com>
2022-03-02 20:25:46 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,12 +1,38 @@
 import PyQt5.QtWidgets as qtw
+import PyQt5.QtGui as qtg
 
 class MainWindow(qtw.QWidget):
 
     def __init__(self):
         super().__init__()
+        # Add title
         self.setWindowTitle("Hello")
 
-        self.show
+        # Set vertical layout
+        self.setLayout(qtw.QVBoxLayout())
+
+        # Create a label
+        my_label = qtw.QLabel("Hello World!, What is your name?")
+        
+        # Change font size of label
+        my_label.setFont(qtg.QFont("Times New Roman", 18))
+        self.layout().addWidget(my_label)
+
+        #Create an entry box
+        my_entry = qtw.QLineEdit()
+        my_entry.setObjectName("name_field")
+        my_entry.setText("Type your name here...")
+        self.layout().addWidget(my_entry)        
+
+        # Create button
+
+        button = qtw.QPushButton("Press me!")
+        self.layout().addWidget(button)   
+        
+
+        
+        
+        self.show()
 
 
 
