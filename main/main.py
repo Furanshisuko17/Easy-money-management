dcac62877e534948b7bed2ee3d37f4907b911416
Furanshisuko17 <darkfran555@gmail.com>
2022-03-04 21:32:09 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Expense:
 
     def __init__(self, name, amount):
@@ -5,6 +8,32 @@ class Expense:
         self.amount = amount
 
 
+def clear_console():
+    command = 'clear'
+    if os.name in ('nt', 'dos'): 
+        command = 'cls'
+    os.system('cl')    
+
+
+def print_all(expense_list):
+    for expense in expense_list:
+        print("Nombre\t\t|\tdinero")
+        print(expense.name, "\t\t|\t", expense.amount, sep='')
+    
+
 expense_list = []
 
+finished = False
 
+clear_console()
+
+while not finished:
+
+    print_all(expense_list)
+
+    name = input("Ingrese el nombre del gasto: ")
+    clear_console()
+    amount = input("Ingrese la cantidad de dinero: ")
+    clear_console()
+    
+    expense_list.append(Expense(name, amount))
